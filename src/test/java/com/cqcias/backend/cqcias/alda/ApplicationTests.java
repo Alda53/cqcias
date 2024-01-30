57dbd7b3b43c9aed64f78cfdb0fc05510851b2a0
Alda53 <caldair99@gmail.com>
2024-01-30 16:02:19 -0600

    first commit

---
+++ src/test/java/com/cqcias/backend/cqcias/alda/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqcias.backend.cqcias.alda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
